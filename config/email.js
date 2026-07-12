fb62db0d69c925933c8b867ab45ea26e215ece9e
Vishal Nanaware <nanawarev41@gmail.com>
2026-07-12 21:19:47 +0530

    Force IPv4 for SMTP

--- config/email.js
+++ config/email.js
@@ -5,17 +5,18 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
 
-transporter.verify(function (error, success) {
+transporter.verify((error) => {
   if (error) {
-    console.log("SMTP ERROR:", error);
+    console.log('SMTP ERROR:', error);
   } else {
-    console.log("SMTP Server Ready");
+    console.log('SMTP Server Ready');
   }
 });
 
